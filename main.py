434fa6b7e1f1ce0792c15f6a8d11212516eda761
Jayan Wadusinghearachchi <jayan.wadusinghearachchi@kognitiv.com>
2025-01-10 23:24:52 +0530

    CROS  configuration

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from admin.exceptions.NotFoundException import NotFoundException
 from admin.routers.facebook_auth_router import facebook_auth_router
 from admin.routers.googl_auth_router import google_auth_router
 from routers.graphql_router import graphql_router
+from fastapi.middleware.cors import CORSMiddleware
 
 from exceptions.exception_handler import (
     duplicate_key_error_handler,
@@ -17,6 +18,18 @@ from exceptions.exception_handler import (
 )
 
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.add_exception_handler(ValueError, value_error_exception_handler)
 app.add_exception_handler(NotFoundException, not_found_exception_handler)
